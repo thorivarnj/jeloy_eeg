2616c5801b22bd02863f315bc105861ac30467e7
glenn.hansen <glenn.hansen@bouvet.no>
2025-02-14 21:40:11 +0100

    Add 10minute recording of brain activites

--- recorder.py
+++ recorder.py
@@ -1,4 +1,5 @@
-import csv, mindwave, time, datetime
+import csv, mindwave, time
+from datetime import datetime, timezone
 
 fs = 100
 T = 1/fs
@@ -6,7 +7,7 @@ T = 1/fs
 print('Hi, give me name of the recording session, for example persons name. Timestamp will be added automatically.')
 session_name = input('Session name: ')
 
-ts = datetime.datetime.now(datetime.UTC).strftime('%Y-%m-%dT%H:%M:%S')
+ts = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%S')
 filename = f'recordings/{session_name}_{ts}.dat'
 
 print(f'Writing to {filename}')
@@ -15,7 +16,7 @@ print('Connecting to Mindwave...')
 headset = mindwave.Headset('/dev/tty.MindWaveMobile')
 
 print('Connected, waiting 10 seconds for data to start streaming')
-time.sleep(10)
+time.sleep(1)
 
 print('Starting to record, automatically recording 10 minute slice so keep on working...')
 f = open(filename, 'w')
@@ -26,7 +27,7 @@ with f:
     writer.writerow(['Timestamp','Raw','Attention','Meditation','delta','theta','low-alpha','high-alpha','low-beta','high-beta','low-gamma','mid-gamma'])
     while time.time() < future:
     #while True:
-        ts = datetime.datetime.now(datetime.UTC).isoformat()
+        ts = datetime.now(timezone.utc).isoformat()
         print ("Raw value: %s, Attention: %s, Meditation: %s" % (headset.raw_value, headset.attention, headset.meditation))
         print ("Waves: {}".format(headset.waves))
         values = list(headset.waves.values())
